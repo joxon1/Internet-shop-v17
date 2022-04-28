76558dc16d49c6709ce53d4c5619779695731dfa
Joxongir Baxtiyarovich <jahongir.r1999@gmail.com>
2022-04-29 04:41:02 +0500

    react --v 17

---
+++ src/pages/Auth.js
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  Button,
+  Card,
+  Container,
+  Form,
+  FormControl,
+  Row,
+} from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { REGISTRATION_ROUTE } from "../utilts/consts";
+
+const Auth = () => {
+  return (
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ height: window.innerHeight - 54 }}
+    >
+      <Card style={{ width: 680 }} className="p-5">
+        <h2 className="m-auto">Авторизация</h2>
+        <Form className="d-flex flex-column">
+          <FormControl className="mt-3" placeholder="Введите ваш email..." />
+          <FormControl className="mt-3" placeholder="Введите ваш парол..." />
+          <Row>
+            <div>
+              Нет аккаунта ?{" "}
+              <NavLink to={REGISTRATION_ROUTE}>Зарегитрируйте</NavLink>
+            </div>
+            <Button className="mt-3 align-self-end" variant={"outline-success"}>
+              Войти
+            </Button>
+          </Row>
+        </Form>
+      </Card>
+    </Container>
+  );
+};
+
+export default Auth;
