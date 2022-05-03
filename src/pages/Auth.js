d1ab10db6d21832929093788217741e61365df5e
rakhimjonovjoxongir@gmail.com <jahongir.r1999@gmail.com>
2022-05-04 02:31:39 +0500

    added file to  dotenv but error

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Button,
   Card,
@@ -8,11 +8,23 @@ import {
   Row,
 } from "react-bootstrap";
 import { NavLink, useLocation } from "react-router-dom";
+import { login, registration } from "../http/userAPI";
 import { lOGIN_ROUTE, REGISTRATION_ROUTE } from "../utilts/consts";
 
 const Auth = () => {
   const location = useLocation();
   const isLogin = location.pathname === lOGIN_ROUTE;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const click = async () => {
+    if (isLogin) {
+      const response = await login();
+    } else {
+      const response = await registration(email,password);
+      console.log(response);
+    }
+  };
 
   return (
     <Container
@@ -22,8 +34,19 @@ const Auth = () => {
       <Card style={{ width: 680 }} className="p-5">
         <h2 className="m-auto">{isLogin ? "Авторизация" : "Регистрация"}</h2>
         <Form className="d-flex flex-column">
-          <FormControl className="mt-3" placeholder="Введите ваш email..." />
-          <FormControl className="mt-3" placeholder="Введите ваш парол..." />
+          <FormControl
+            className="mt-3"
+            placeholder="Введите ваш email..."
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <FormControl
+            className="mt-3"
+            placeholder="Введите ваш парол..."
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+          />
           <Row className="d-flex justify-content-between mt-3 ">
             {isLogin ? (
               <div>
@@ -32,11 +55,12 @@ const Auth = () => {
               </div>
             ) : (
               <div>
-                Есть аккаунта ?{" "}
-                <NavLink to={lOGIN_ROUTE}>Войдите!</NavLink>
+                Есть аккаунта ? <NavLink to={lOGIN_ROUTE}>Войдите!</NavLink>
               </div>
             )}
-            <Button variant={"outline-success"}>{isLogin ? "Войти" : "Регистрация"}</Button>
+            <Button variant={"outline-success"} onClick={() => click}>
+              {isLogin ? "Войти" : "Регистрация"}
+            </Button>
           </Row>
         </Form>
       </Card>
