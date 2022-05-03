b18cfebefd7f015a8d6cb443c7f4f00fe5e40362
joxon1 <jahongir.r1999@gmail.com>
2022-05-03 16:21:31 +0500

    admin panel

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,25 +2,53 @@ import React, { useContext } from "react";
 import { Context } from "../index";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import { SHOP_ROUTE } from "../utilts/consts";
+import { ADMIN_ROUTE, lOGIN_ROUTE, SHOP_ROUTE } from "../utilts/consts";
 import { Button, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { observer } from "mobx-react-lite";
+import { useHistory } from "react-router-dom";
 
 const NavBar = observer(() => {
   const { user } = useContext(Context);
+  const history = useHistory();
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Link style={{ color: "white" }} to={SHOP_ROUTE}>-Telshop- </Link>
-        {user.isAuth ? <Nav className="d-flex justify-content-evenly" style={{ color: "white" }} >
-          <Button variant={"outline-light"} className="me-2">Админ панель</Button>
-          <Button variant={"outline-light"} >Выйти</Button>
-        </Nav>
-        : <Nav className="d-flex justify-content-evenly" style={{ color: "white" }} >
-        <Button variant={"outline-light"}onClick={()=>user.setIsAuth(true)}>Авторизация</Button>
-      </Nav>
-      }
+        <Link style={{ color: "white" }} to={SHOP_ROUTE}>
+          -Telshop-{" "}
+        </Link>
+        {user.isAuth ? (
+          <Nav
+            className="d-flex justify-content-evenly"
+            style={{ color: "white" }}
+          >
+            <Button
+              variant={"outline-light"}
+              onClick={() => history.push(ADMIN_ROUTE)}
+              className="me-2"
+            >
+              Админ панель
+            </Button>
+            <Button
+              variant={"outline-light"}
+              onClick={() => history.push(lOGIN_ROUTE)}
+            >
+              Выйти
+            </Button>
+          </Nav>
+        ) : (
+          <Nav
+            className="d-flex justify-content-evenly"
+            style={{ color: "white" }}
+          >
+            <Button
+              variant={"outline-light"}
+              onClick={() => user.setIsAuth(true)}
+            >
+              Авторизация
+            </Button>
+          </Nav>
+        )}
       </Container>
     </Navbar>
   );
